ed39d77569d60925a78893eb369c83357d03d40a
GC Chen <cgc.elvin@outlook.com>
2025-07-11 18:11:35 +0800

    Add reconnect codes.

--- Src/NanoRocket/RocketConsumerService.cs
+++ Src/NanoRocket/RocketConsumerService.cs
@@ -6,7 +6,7 @@ using Org.Apache.Rocketmq;
 
 namespace NanoRocket;
 
-public class RocketConsumerService : IHostedService,  IDisposable
+public class RocketConsumerService : IHostedService, IDisposable
 {
     private readonly ILogger<RocketConsumerService> _logger;
     private readonly IConfiguration _configuration;
@@ -34,7 +34,7 @@ public class RocketConsumerService : IHostedService,  IDisposable
         try
         {
             cancellationToken.Register(() => _consumerCts.Cancel());
-            
+
             var config = _configuration.GetSection(_configKey);
             var consumerGroup = config.GetValue<string>("ConsumerGroup") ??
                                 throw new Exception("ConsumerGroup should not be null");
@@ -46,62 +46,87 @@ public class RocketConsumerService : IHostedService,  IDisposable
             var messageHandler = _serviceProvider.GetKeyedService<IMessageHandler>(_handlerKey) ??
                                  throw new Exception($"Could not find handler by handlerKey: {_handlerKey}");
 
-            _logger.LogInformation($"ConsumerGroup [{consumerGroup}] is connecting RocketMQ, Topic: [{topic}]...");
-
-            // Enable the switch if you use .NET Core 3.1 and want to disable TLS/SSL.
-            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-
-
-            // Add your subscriptions.
-            var subscription = new Dictionary<string, FilterExpression>
-                { { topic, new FilterExpression("*") } };
-            // In most case, you don't need to create too many consumers, single pattern is recommended.
-            var simpleConsumerBuilder = new SimpleConsumer.Builder()
-                .SetConsumerGroup(consumerGroup)
-                .SetAwaitDuration(TimeSpan.FromSeconds(15))
-                .SetSubscriptionExpression(subscription);
-            // Credential provider is optional for client configuration.
-            var clientConfigBuilder = new ClientConfig.Builder()
-                .SetEndpoints(endpoints)
-                .EnableSsl(false);
-            if (!string.IsNullOrEmpty(accessKey) && !string.IsNullOrEmpty(secretKey))
-            {
-                var credentialsProvider = new StaticSessionCredentialsProvider(accessKey, secretKey);
-                clientConfigBuilder
-                    .SetCredentialsProvider(credentialsProvider);
-            }
-
-            var clientConfig = clientConfigBuilder.Build();
-            simpleConsumerBuilder.SetClientConfig(clientConfig);
-
-            _simpleConsumer = await simpleConsumerBuilder.Build();
-            
-            _logger.LogInformation($"{endpoints}|{topic}|Consumer initialized");
-            
             _ = Task.Run(async () =>
             {
                 while (!_consumerCts.Token.IsCancellationRequested)
                 {
                     try
                     {
-                        // Receive messages
-                        var messageViews = await _simpleConsumer.Receive(16, TimeSpan.FromSeconds(15));
-                        if (messageViews != null && messageViews.Any())
+                        _logger.LogInformation(
+                            $"ConsumerGroup [{consumerGroup}] is connecting RocketMQ, Topic: [{topic}]...");
+
+                        // Enable the switch if you use .NET Core 3.1 and want to disable TLS/SSL.
+                        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+
+                        // Add your subscriptions.
+                        var subscription = new Dictionary<string, FilterExpression>
+                            { { topic, new FilterExpression("*") } };
+                        // In most case, you don't need to create too many consumers, single pattern is recommended.
+                        var simpleConsumerBuilder = new SimpleConsumer.Builder()
+                            .SetConsumerGroup(consumerGroup)
+                            .SetAwaitDuration(TimeSpan.FromSeconds(15))
+                            .SetSubscriptionExpression(subscription);
+                        // Credential provider is optional for client configuration.
+                        var clientConfigBuilder = new ClientConfig.Builder()
+                            .SetEndpoints(endpoints)
+                            .EnableSsl(false);
+                        if (!string.IsNullOrEmpty(accessKey) && !string.IsNullOrEmpty(secretKey))
                         {
-                            foreach (var message in messageViews)
+                            var credentialsProvider = new StaticSessionCredentialsProvider(accessKey, secretKey);
+                            clientConfigBuilder
+                                .SetCredentialsProvider(credentialsProvider);
+                        }
+
+                        var clientConfig = clientConfigBuilder.Build();
+                        simpleConsumerBuilder.SetClientConfig(clientConfig);
+
+                        _simpleConsumer = await simpleConsumerBuilder.Build();
+
+                        _logger.LogInformation($"{endpoints}|{topic}|Consumer initialized");
+                        
+                        return;
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError($"Failed to connect to RocketMQ: {e.Message}");
+                        await Task.Delay(TimeSpan.FromMinutes(1), _consumerCts.Token);
+                    }
+                }
+            }, _consumerCts.Token);
+
+            _ = Task.Run(async () =>
+            {
+                while (!_consumerCts.Token.IsCancellationRequested)
+                {
+                    try
+                    {
+                        if (_simpleConsumer != null)
+                        {
+                            // Receive messages
+                            var messageViews = await _simpleConsumer.Receive(16, TimeSpan.FromSeconds(15));
+                            if (messageViews != null && messageViews.Any())
                             {
-                                await messageHandler.HandleMessage(message);
-                                _logger.LogInformation(
-                                    $"[{_configKey}] Received a message, topic={message.Topic}, message-id={message.MessageId}, body-size={message.Body.Length}");
-                                await _simpleConsumer.Ack(message);
-                                _logger.LogInformation($"[{_configKey}] Message is acknowledged successfully, message-id={message.MessageId}");
-                                // await simpleConsumer.ChangeInvisibleDuration(message, TimeSpan.FromSeconds(15));
-                                // _logger.LogInformation($"Changing message invisible duration successfully, message=id={message.MessageId}");
+                                foreach (var message in messageViews)
+                                {
+                                    await messageHandler.HandleMessage(message);
+                                    _logger.LogInformation(
+                                        $"[{_configKey}] Received a message, topic={message.Topic}, message-id={message.MessageId}, body-size={message.Body.Length}");
+                                    await _simpleConsumer.Ack(message);
+                                    _logger.LogInformation(
+                                        $"[{_configKey}] Message is acknowledged successfully, message-id={message.MessageId}");
+                                    // await simpleConsumer.ChangeInvisibleDuration(message, TimeSpan.FromSeconds(15));
+                                    // _logger.LogInformation($"Changing message invisible duration successfully, message=id={message.MessageId}");
+                                }
+                            }
+                            else
+                            {
+                                await Task.Delay(TimeSpan.FromSeconds(1), _consumerCts.Token);
                             }
                         }
                         else
                         {
-                            await Task.Delay(TimeSpan.FromSeconds(1), _consumerCts.Token);
+                            await Task.Delay(TimeSpan.FromMinutes(1), _consumerCts.Token);
                         }
                     }
                     catch (OperationCanceledException)
@@ -115,9 +140,10 @@ public class RocketConsumerService : IHostedService,  IDisposable
                         await Task.Delay(TimeSpan.FromSeconds(5), _consumerCts.Token);
                     }
                 }
+
                 _logger.LogInformation($"[{_configKey}] Consumer receive loop exited.");
             }, _consumerCts.Token);
-            
+
 
             _logger.LogInformation("Run RocketMQ Consumer succeed");
             await Task.CompletedTask;
@@ -128,14 +154,13 @@ public class RocketConsumerService : IHostedService,  IDisposable
             throw;
         }
     }
-    
-    
+
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation($"RocketMQ Consumer [{_configKey}] stopping");
         await _consumerCts.CancelAsync();
-        
+
         if (_simpleConsumer != null)
         {
             try
