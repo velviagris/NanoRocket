daa610748afbe3f920914480ea00704c87fba663
GC Chen <cgc.elvin@outlook.com>
2025-07-11 18:12:03 +0800

    Add Example.ReadSettingConsumer

---
+++ Example/Example.ReadSettingConsumer/FooTopicHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using NanoRocket;
+using Org.Apache.Rocketmq;
+
+namespace Example.ReadSettingConsumer;
+
+public class FooTopicHandler : IMessageHandler
+{
+    public async Task HandleMessage(MessageView message)
+    {
+        Console.WriteLine(Encoding.UTF8.GetString(message.Body));
+
+        await Task.Delay(1000);
+    }
+}
---
+++ Example/Example.ReadSettingConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Example.ReadSettingConsumer;
+using Microsoft.Extensions.Hosting;
+using NanoRocket.DependencyInjection;
+using NLog;
+
+
+var logger = LogManager.GetCurrentClassLogger();
+logger.Debug("init main");
+
+try
+{
+    var app = CreateHostBuilder(args).Build();
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    logger.Error(ex, ex.Message);
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
+
+IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            // 注入 rocketmq 消费者
+            services.AddRocketConsumerServiceFromAppSettings<FooTopicHandler>("FooTopicHandlerConfig");
+        });
